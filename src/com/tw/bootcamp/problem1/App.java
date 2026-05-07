63e3a36ab17c4da1c0d3446b4b23a63af31c62f3
Umar Khaji <umarkhaji@Umars-MacBook-Pro.local>
2026-05-07 12:02:13 +0530

    Integrates Square class into the application entry point * Imports the Square class from the shapes package * Instantiates a new Square object with a side length of 10 * Prints the calculated area and perimeter of the square to the console

--- src/com/tw/bootcamp/problem1/App.java
+++ src/com/tw/bootcamp/problem1/App.java
@@ -1,11 +1,16 @@
 package com.tw.bootcamp.problem1;
 
 import com.tw.bootcamp.problem1.shapes.Rectangle;
+import com.tw.bootcamp.problem1.shapes.Square;
 
 public class App {
   static void main() {
     Rectangle rectangle = new Rectangle(15, 10);
     System.out.println(rectangle.area());
     System.out.println(rectangle.perimeter());
+
+    Square square = new Square(10);
+    System.out.println(square.area());
+    System.out.println(square.perimeter());
   }
 }
