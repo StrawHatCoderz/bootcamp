7f7b3bc6a1f69fb7327a078803a0f94fa7416578
Umar Khaji <umarkhaji@Umars-MacBook-Pro.local>
2026-05-07 11:19:16 +0530

    Adds JUnit 5 test case template to AppTest * Imports the JUnit Jupiter Test annotation * Adds a sample test method to the AppTest class to facilitate new test implementations

---
+++ src/com/tw/bootcamp/problem1/App.java
@@ -0,0 +1,4 @@
+package com.tw.bootcamp.problem1;
+
+public class App {
+}
---
+++ test/com/tw/bootcamp/problem1/AppTest.java
@@ -0,0 +1,17 @@
+package com.tw.bootcamp.problem1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+  @org.junit.jupiter.api.BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void sampleTest() {
+    
+  }
+}
