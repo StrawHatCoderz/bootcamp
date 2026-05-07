0531cdfe21afa248e711b341d29b9e9c8c966221
Umar Khaji <umarkhaji@Umars-MacBook-Pro.local>
2026-05-07 15:34:52 +0530

    Renames probability method to of and updates corresponding test cases * Renames the probability method to of in the Probability class * Updates all test method names to reflect the method name change * Adjusts assertions in ProbabilityTest to call the new method name * Synchronizes compiled test classes with the source code changes

---
+++ src/com/tw/bootcamp/problem2/Probability.java
@@ -0,0 +1,7 @@
+package com.tw.bootcamp.problem2;
+
+public class Probability {
+  public double of(double favourOutcomes, int noOfChances) {
+    return favourOutcomes / noOfChances;
+  }
+}
--- src/com/tw/bootcamp/problem2/ProbabilityCalculator.java
+++
@@ -1,7 +0,0 @@
-package com.tw.bootcamp.problem2;
-
-public class ProbabilityCalculator {
-  public double probability(double favourOutcomes, int noOfChances) {
-    return favourOutcomes / noOfChances;
-  }
-}
